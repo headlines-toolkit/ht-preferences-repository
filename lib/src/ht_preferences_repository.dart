42984f1fee10ca450d5e375813d71b9e3778e1e8
fulleni <fulleni@hotmail.com>
2025-04-11 10:03:30 +0100

    chore: init

---
+++ lib/src/ht_preferences_repository.dart
@@ -0,0 +1,7 @@
+/// {@template ht_preferences_repository}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class HtPreferencesRepository {
+  /// {@macro ht_preferences_repository}
+  const HtPreferencesRepository();
+}
---
+++ test/src/ht_preferences_repository_test.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:ht_preferences_repository/ht_preferences_repository.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('HtPreferencesRepository', () {
+    test('can be instantiated', () {
+      expect(HtPreferencesRepository(), isNotNull);
+    });
+  });
+}
